a9a4be9518be840dea001dab266021fd5b3590a8
ShayekhBinIslam <38907457+ShayekhBinIslam@users.noreply.github.com>
2020-01-26 16:03:23 +0600

    scraped from Espn Cricinfo

---
+++ regression/tamim_t20_jan_2020_cricinfo.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 26 15:14:54 2020
+
+@author: Shayekh_BUET1705009
+"""
+
+url = 'http://stats.espncricinfo.com/ci/engine/player/56194.html?class=3;template=results;type=allround;view=match'
+      
+from requests import get
+response = get(url)
+print(response.text[:500])
+
+from bs4 import BeautifulSoup
+html_soup = BeautifulSoup(response.text, 'html.parser')
+type(html_soup)
+
+
+tables = html_soup.find_all('table')
+caption = 'Match by match list'
+
+data = tables[3].find('tbody').find_all('tr')
+relevant_cols = [0, 6, 7, 8]
+col_lables = ['runs', 'opposition', 'ground', 'date']
+runs = []
+opposition = []
+ground = []
+date = []
+
+for rows in data:
+    row = rows.find_all('td')
+    runs.append(row[0].text.replace('*', ''))
+    opposition.append(row[6].text[2:])
+    ground.append(row[7].text)
+    date.append(row[8].text)
+
+date = [pd.to_datetime(a, format='%Y-%m-%d', infer_datetime_format=True) for a in date]
+#from datetime import datetime
+date = [a.strftime('%Y-%m-%d') for a in date]
+
+import pandas as pd
+df = pd.DataFrame({'date': date, 'runs': runs,\
+                   'opposition': opposition, 'ground': ground})
+
+
+df.to_csv('tamim_t20_jan_2020_stat_cricinfo.csv', \
+          index=False, date_format='%m-%d-%Y')
+
